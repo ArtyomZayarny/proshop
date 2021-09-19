aa32e7990b4e56b9ccd2c48a2e8e0eae6e772146
ArtyomZayarny <artyom.zayarny@gmail.com>
2021-09-19 12:22:51 +0300

    Init server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MODE = process.env.NODE_ENV || "development";
+
+app.listen(PORT, console.log(`Server running in ${MODE} mode on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("Api is running...");
+});
