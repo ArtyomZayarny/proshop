3e4eaf132aedc634d81fc64de9c8f1b03819a142
ArtyomZayarny <artyom.zayarny@gmail.com>
2021-09-19 13:55:22 +0300

    connect app to db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
 
 const app = express();
+dotenv.config();
+connectDB();
 const PORT = process.env.PORT || 5000;
 const MODE = process.env.NODE_ENV || "development";
 
-app.listen(PORT, console.log(`Server running in ${MODE} mode on port ${PORT}`));
-console.log("server");
+app.listen(
+  PORT,
+  console.log(`Server running in ${MODE} mode on port ${PORT}`.yellow.bold)
+);
 
 app.get("/", (req, res) => {
   res.send("Api is running...");
