40a3e5a031b058992d423477dc80a96f358541bb
ArtyomZayarny <artyom.zayarny@gmail.com>
2021-09-19 12:35:55 +0300

    Init client and start scripts

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 5000;
 const MODE = process.env.NODE_ENV || "development";
 
 app.listen(PORT, console.log(`Server running in ${MODE} mode on port ${PORT}`));
+console.log("server");
 
 app.get("/", (req, res) => {
   res.send("Api is running...");
